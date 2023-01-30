7377fb6cddbd11d5f76e420f3625097eadc03d83
Emil1577 <emilronquillo@gmail.com>
2023-01-29 22:48:23 -0800

    working on functions

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
-
 const app = express();
-const PORT = process.env.port || 3001;
 const path = require('path');
+const notesData = require('./db/db.json');
 
-const db = [];
+const PORT = process.env.port || 3001;
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
